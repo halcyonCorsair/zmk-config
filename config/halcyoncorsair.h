8bc3d3bfbd60c47f301030cc19db17d92b80ec53
Elliot Pahl <elliot.pahl@gmail.com>
2022-02-15 01:10:51 +1100

    WIP

--- config/halcyoncorsair.h
+++ config/halcyoncorsair.h
@@ -1,24 +1,38 @@
 #define _BASE 0
-#define _NUMB 1
-#define _FUNC 2
-#define _NAVI 3
+#define _SYMB 1
+#define _NAVI 2
+#define _FUNC 3
 #define _SYST 4
 
 #define NAV_ENT _NAVI ENTER
-#define NUM_BCK _NUMB BACKSPACE
+#define SYM_BCK _SYMB BACKSPACE
 
-#define X_SELA      LC(A)
-#define X_UNDO      LC(Z)
-#define X_CUT       LC(X)
-#define X_COPY      LC(C)
-#define X_PAST      LC(V)
-#define X_REDO      LC(Y)
+#define NAV_BCK _NAVI BACKSPACE
+#define SYM_ENT _NAVI ENTER
+
+#define X_SELA      LG(A)
+#define X_UNDO      LG(Z)
+#define X_CUT       LG(X)
+#define X_COPY      LG(C)
+#define X_PAST      LG(V)
+#define X_REDO      LG(LS(Z))
 
 #define X_A LCTRL   A
 #define X_R LALT    R
 #define X_S LGUI    S
 #define X_T LSHFT   T
+
 #define X_N RSHFT   N
 #define X_E RGUI    E
 #define X_I LALT    I
 #define X_O RCTRL   O
+
+#define X_DLR LCTRL     A
+#define X_PLUS LALT     R
+#define X_LPRN LGUI     S
+#define X_RPRN LSHFT    T
+
+#define X_MINUS RSHFT   N
+#define X_EQUAL RGUI    E
+#define X_UNDER LALT    I
+#define X_ASTRK RCTRL   O
